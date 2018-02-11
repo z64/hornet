bfded81d67a2644fb94a0ac9e5e7c54255cb4db1
Zac Nowicki <zachnowicki@gmail.com>
2018-02-11 09:01:07 +0000

    Add response time to ping

--- src/hornet/features/ping.cr
+++ src/hornet/features/ping.cr
@@ -1,7 +1,12 @@
 module Hornet
   client.stack(:ping, Flipper.new("ping")) do |ctx|
-    if ctx.message.content == "<@213450769276338177> ping"
-      client.create_message(ctx.message.channel_id, "pong")
+    if ctx.message.content == "<@213450769276338177> \u{1f3d3}"
+      message = nil
+      time = Time.measure do
+        message = client.create_message(ctx.message.channel_id, "\u{1f3d3}")
+      end
+
+      client.edit_message(ctx.message.channel_id, message.as(Discord::Message).id, "\u{1f3d3} `#{time}`")
     end
   end
 end
