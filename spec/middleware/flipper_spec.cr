b5af57497ad65beb42c5f42eb2042a8fefe56d92
Zac Nowicki <zachnowicki@gmail.com>
2018-08-18 21:07:01 -0400

    [Flipper] Correct yield condition
    Specs: Spec Flipper#call

--- spec/middleware/flipper_spec.cr
+++ spec/middleware/flipper_spec.cr
@@ -1,8 +1,12 @@
 require "../spec_helper"
 
+class Hornet::Flipper
+  include StubbedCachedRoutes
+end
+
 describe Hornet::Flipper do
-  Hornet::Flipper.new("foo")
-  Hornet::Flipper.new("bar")
+  foo_flipper = Hornet::Flipper.new("foo")
+  bar_flipper = Hornet::Flipper.new("bar")
 
   it "stores instanced features" do
     Hornet::Flipper.features.should eq ["foo", "bar"]
@@ -18,5 +22,23 @@ describe Hornet::Flipper do
     Hornet::Flipper.disable("foo", 1)
     result = Hornet::FlipperSettings.get("foo", 1)
     result.not_nil!.enabled.should be_false
+  end
+
+  it "#call on enabled guild" do
+    Hornet::Flipper.enable("foo", 1)
+    Hornet::Flipper.disable("bar", 1)
+    ctx = {Discord::Client => :client}
+
+    foo_called = false
+    foo_flipper.call(MessageStub.new(1, "content"), ctx) do
+      foo_called = true
+    end
+    foo_called.should be_true
+
+    bar_called = false
+    bar_flipper.call(MessageStub.new(1, "content"), ctx) do
+      bar_called = true
+    end
+    bar_called.should be_false
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -4,6 +4,14 @@ require "../src/hornet"
 record MessageStub, channel_id : UInt64, content : String
 record MessageWithEmbedStub, channel_id : UInt64, content : String, embed : Discord::Embed
 
+module StubbedCachedRoutes
+  record CachedChannel, guild_id : UInt64
+
+  def get_channel(_client, channel_id)
+    CachedChannel.new(guild_id: 1)
+  end
+end
+
 class MockClient
   def on_message_create(&block : MessageStub ->)
     block
